0d1bd70ba837f722b0f54957047e34b34f2014b4
Sophie N <77267544+Snourry-bis@users.noreply.github.com>
2021-04-22 17:31:47 +0200

    modif corps site

---
+++ src/components/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+class Home extends React.Component {
+    render() {
+        return (
+            <div style={{marginTop: '100px', textAlign: 'center'}}>
+                <h3>Bienvenue sur Lift Board Game</h3>
+                <h4>Les jeux de l'ascenseur</h4>
+            </div>
+        );
+    }
+}
+
+export default Home;
---
+++ src/components/Navbar.js
@@ -0,0 +1,147 @@
+import React from 'react'
+
+import {makeStyles, useTheme} from '@material-ui/core/styles';
+import Drawer from '@material-ui/core/Drawer';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Divider from '@material-ui/core/Divider';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import ChevronRightIcon from '@material-ui/icons/ChevronRight';
+import {Link} from '@material-ui/core';
+
+const drawerWidth = 240;
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        display: 'flex',
+    },
+    appBar: {
+        transition: theme.transitions.create(['margin', 'width'], {
+            easing: theme.transitions.easing.sharp,
+            duration: theme.transitions.duration.leavingScreen,
+        }),
+        backgroundColor: 'rgb(158,255,205)',
+    },
+    appBarShift: {
+        width: `calc(100% - ${drawerWidth}px)`,
+        marginLeft: drawerWidth,
+        transition: theme.transitions.create(['margin', 'width'], {
+            easing: theme.transitions.easing.easeOut,
+            duration: theme.transitions.duration.enteringScreen,
+        }),
+    },
+    menuButton: {
+        marginRight: theme.spacing(2),
+    },
+    hide: {
+        display: 'none',
+    },
+    drawer: {
+        width: drawerWidth,
+        flexShrink: 0,
+    },
+    drawerPaper: {
+        width: drawerWidth,
+        backgroundColor: 'rgb(158,255,205)',
+    },
+    drawerHeader: {
+        display: 'flex',
+        alignItems: 'center',
+        padding: theme.spacing(0, 1),
+        // necessary for content to be below app bar
+        ...theme.mixins.toolbar,
+        justifyContent: 'flex-end',
+    },
+    content: {
+        flexGrow: 1,
+        padding: theme.spacing(3),
+        transition: theme.transitions.create('margin', {
+            easing: theme.transitions.easing.sharp,
+            duration: theme.transitions.duration.leavingScreen,
+        }),
+        marginLeft: -drawerWidth,
+    },
+    contentShift: {
+        transition: theme.transitions.create('margin', {
+            easing: theme.transitions.easing.easeOut,
+            duration: theme.transitions.duration.enteringScreen,
+        }),
+        marginLeft: 0,
+    },
+    link: {
+        color: 'white'
+    },
+    button: {
+        marginLeft: '20px',
+        paddingLeft: '20px'
+    }
+
+}));
+
+export default function Navbar() {
+    const classes = useStyles();
+    const theme = useTheme();
+    const [open, setOpen] = React.useState(false);
+
+    const handleDrawerOpen = () => {
+        setOpen(true);
+    };
+
+    const handleDrawerClose = () => {
+        setOpen(false);
+    };
+
+    return (
+        <div className={classes.root}>
+            <CssBaseline/>
+            <AppBar
+                style={{backgroundColor: 'rgb(48,235,137)'}}
+                position="fixed"
+                className={classes.appBar}
+            >
+                <Toolbar>
+                    <IconButton
+                        color="inherit"
+                        aria-label="open drawer"
+                        onClick={handleDrawerOpen}
+                        edge="start"
+                        className={classes.menuButton}
+                    >
+                        <MenuIcon/>
+                    </IconButton>
+                    <Link className={classes.link} href="/">
+                        <Typography variant="h6" noWrap>
+                            Lift Board Game
+                        </Typography>
+                    </Link>
+                </Toolbar>
+            </AppBar>
+            <Drawer
+                className={classes.drawer}
+                variant="persistent"
+                anchor="left"
+                open={open}
+                classes={{
+                    paper: classes.drawerPaper,
+                }}
+            >
+                <div className={classes.drawerHeader}>
+                    <IconButton onClick={handleDrawerClose}>
+                        {theme.direction === 'ltr' ? <ChevronLeftIcon/> : <ChevronRightIcon/>}
+                    </IconButton>
+                </div>
+                <Divider/>
+
+                <div className={classes.button}>
+                    {/*<Link className={classes.link} href=""><Typography>Nouveau rêve</Typography></Link>
+                    <Link className={classes.link} href=""><Typography>A propos</Typography></Link>
+                    <Link className={classes.link} href=""><Typography>Report bug</Typography></Link>*/}
+                </div>
+            </Drawer>
+        </div>
+    );
+}
